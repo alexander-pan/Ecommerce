0d76193ea4bc3a0addda34b36002fd6d264e7241
Alexander V Pan <35742839+alexander-pan@users.noreply.github.com>
2018-05-18 12:47:06 -0400

    Updated Functions

--- functions.py
+++ functions.py
@@ -1,5 +1,11 @@
 import numpy as np
 
+#convert Ilink
+def convertIlink(ilink):
+    s = str(ilink)
+    r = 10-len(s)
+    return '0'*r + s
+
 #get frequency totals for specific departments: woven shirts, dresses, knit tops, and pants
 def getTotalFreq(dataframe,dept):
     x = dataframe.groupby('DEPARTMENT_NAME').size().to_dict()
@@ -197,7 +203,7 @@ def AvgGrossMargin(dataframe):
     costs = dataframe.SHIPPED_COST_AMT
     gross_margin = (sold-costs)/sold
     return gross_margin.mean()
-
+"""
 def AvgLifeSpan(dataframe):
     years = [2014,2015,2016,2017]
     n = 1
@@ -219,3 +225,96 @@ def CLV(dataframe):
     alt = AvgLifeSpan(dataframe)
     agm = AvgGrossMargin(dataframe)
     return (T*aov)*agm*alt
+"""
+
+def getTableRating(ilinks,dataframe,cols):
+    """
+    cols = ['Ilink','N_ws','n_ws','S_ws','s_ws','si_ws','D_ws','d_ws','di_ws','R_ws',
+        'N_kt','n_kt','S_kt','s_kt','si_kt','D_kt','d_kt','di_kt','R_kt',
+        'N_d','n_d','S_d','s_d','si_d','D_d','d_d','di_d','R_d',
+        'N_p','n_p','S_p','s_p','si_p','D_p','d_p','di_p','R_p']
+    """
+    tab = pd.DataFrame([],columns=cols)
+    #need to create rows that describe a customers freq and sales of items in specific departments
+    depts = ['Woven Shirts','Knit Tops','Dresses','Pants']
+    K = 4 #Number of Depts/topics
+    for ilink in ilinks:
+        dfCust = dataframe.loc[(dataframe.ILINK==ilink)]
+        if ~dfCust.empty:
+            N_u = dfCust.shape[0]#sum([f.getTotalFreq(dfCust,dept) for dept in depts])
+            #print ilink,N_u
+            fts = []
+            for dept in depts:
+                N = f.getTotalFreq(dfCust,dept)
+                n = f.getAvgFreqOrder(dfCust,dept)
+                O,Q,G,S,D = f.getTotalAmts(dfCust,dept)
+                o,q,g,s,d = f.getAvgAmtsOrder(dfCust,dept)
+                oi,qi,gi,si,di = f.getAvgAmtItem(dfCust,dept)
+                alpha = [N,n,S,s,si,D,d,di]
+                #alpha = [S,s,si,D,d,di]
+                #alpha = [N,S,s,si]
+                alpha = [0 if math.isnan(x) else x for x in alpha]
+                alpha1 = np.array(normalize(alpha))
+                sigma = UserSigma[ilink][dept]
+                #sigma = (N + alpha1)/(N_u + K*alpha1)
+                pref = np.dot(sigma,alpha1).round(2)
+                #print ilink,alpha
+                #print sigma
+                #print ''
+                fts.append(alpha+[pref])
+                #fts2.append(alpha)
+        else:
+            pass
+        row = [i for sub in fts for i in sub]
+        row.insert(0,ilink)
+        temp = pd.DataFrame([tuple(row)],columns=cols)
+        tab = pd.concat([tab,temp])
+    return tab
+
+#This function will get the customers highest Rating pref
+#It checks the rating first
+#if ratings are equal it checks for freq, total sold/sales as tiebreakers in that order
+#if that is not a tie-breaker it will return as many dept as "preferred" and consider that user as 
+#having multiple preferences
+def getHigherPref(row):
+    ws = (row.R_ws,row.N_ws,row.S_ws)
+    kt = (row.R_kt,row.N_kt,row.S_kt)
+    d = (row.R_d,row.N_d,row.S_d)
+    p = (row.R_p,row.N_p,row.S_p)
+    R = {'Woven Shirts': ws, 'Knit Tops': kt, 'Dresses': d, 'Pants': p}
+    maxNum = 0.0
+    maxKey = []
+    #get those with highest rating first
+    for key,value in R.iteritems():
+        if value[0] > maxNum:
+            maxNum = value[0]
+            maxKey.append(key)
+        elif value[0] == maxNum and value[0] != 0:
+            maxKey.append(key)
+    
+    #if no purchases were made
+    if len(maxKey) == 0:
+        return 0
+    #if rating are equal, then look at freq purchased for pref
+    i = 1
+    while len(maxKey) != 1:
+        RTop = {x:R[x] for x in maxKey}
+        n = 0
+        maxKey = []
+        for key,value in RTop.iteritems():
+            #print value[i]
+            if value[i] > n:
+                n = value[i]
+                if len(maxKey)==0:
+                    maxKey.append(key)
+                else:
+                    maxKey.pop()
+                    maxKey.append(key)
+            elif value[i] == n and value[i] !=0:
+                maxKey.append(key)
+        #print maxKey,n
+        if len(maxKey) > 1:
+            i += 1
+            if i > 2:
+                return maxKey
+    return maxKey[0]
